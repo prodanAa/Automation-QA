78a67489d26f5f464c9e53b57c64fc8af96103d5
Prodan <34188423+prodanAa@users.noreply.github.com>
2022-06-15 22:46:08 +0300

    WebDriverTests
    WebDriverTests With SELENIUM WEB DRIVER

---
+++ WebDriverTestsDemo/WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+//Create new Chrome Browser Instance
+var driver = new ChromeDriver();
+
+//Navigate to Wikipedia
+driver.Navigate().GoToUrl("Http://wikipedia.org");
+
+Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+//Locate Search Feeld by ID
+var searchField = driver.FindElement(By.Id("searchInput")); 
+
+//Click on Element
+searchField.Click();
+
+//Fill with QA
+searchField.SendKeys("QA" + Keys.Enter);
+
+Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+// Close the Browser
+driver.Quit();
